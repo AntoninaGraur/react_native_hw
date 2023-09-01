94cf9761eccbd93875b4248a996c00fefc177512
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-09-01 21:06:57 +0200

    regist form almost donne

--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -1,20 +1,42 @@
-import React from "react";
-import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, StyleSheet, Image } from "react-native";
 import RegisterButton from "./components/Buttons";
 
 const RegistrationScreen = () => {
+  const [password, setPassword] = useState("");
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible(!isPasswordVisible);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Registration </Text>
-      <TextInput style={styles.input} placeholder="Name" />
-      <TextInput style={styles.input} placeholder="Email adress" />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry={true}
-      />
-    <RegisterButton/>
-    </View>
+    <>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../assets/photo_bcg.png")}
+          style={styles.backgroundImage}
+        />
+        <View style={styles.container}>
+          <Text style={styles.title}>Registration</Text>
+          <TextInput style={styles.input} placeholder="Login" />
+          <TextInput style={styles.input} placeholder="Email adress" />
+          <View style={styles.passwordContainer}>
+            <TextInput
+              style={styles.passwordInput}
+              placeholder="Password"
+              secureTextEntry={!isPasswordVisible}
+            />
+            <Text
+              style={styles.togglePasswordButton}
+              onPress={togglePasswordVisibility}
+            >
+              {isPasswordVisible ? "Hide" : "Show"}</Text>
+          </View>
+          <RegisterButton />
+        </View>
+      </View>
+    </>
   );
 };
 
@@ -23,19 +45,62 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    backgroundColor: "white",
+    marginTop: 235,
+    width: 380,
+  },
+  imageContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 15,
   },
   title: {
-    fontSize: 24,
-    marginBottom: 20,
+    fontSize: 30,
+    fontWeight: "500",
+    marginBottom: 33,
   },
   input: {
-    width: "80%",
-    height: 40,
-    borderColor: "gray",
+    width: 343,
+    height: 50,
+    borderColor: "#E8E8E8",
     borderWidth: 1,
-    marginBottom: 10,
-      paddingHorizontal: 10,
-    borderRadius: 100,
+    marginBottom: 16,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    backgroundColor: "#F6F6F6",
+  },
+  passwordContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 43,
+  },
+  passwordInput: {
+    width: 343,
+    height: 50,
+    borderColor: "#E8E8E8",
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    backgroundColor: "#F6F6F6",
+    position: "relative",
+  },
+  togglePasswordButton: {
+    padding: 10,
+    fontWeight: "400",
+    position: "absolute",
+    left: 280,
+    color: "#1B4371",
+    fontSize: 16,
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: "cover",
+    position: "absolute",
+    width: "100%",
+    height: "100%",
   },
 });
 
--- Screens/components/Buttons.js
+++ Screens/components/Buttons.js
@@ -1,7 +1,33 @@
 import React from "react";
-import { View, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, Button, TouchableOpacity, StyleSheet } from "react-native";
 
 const RegisterButton = () => {
-  return <Button title="Register" onPress={() => {}} />;
+  return (
+    <TouchableOpacity style={btnStyle.container}>
+      <Text style={btnStyle.title}>Register</Text>
+    </TouchableOpacity>
+  );
+ 
 };
 export default RegisterButton;
+
+
+const btnStyle = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderColor: "transparent",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FF6C00",
+    borderRadius: 100,
+    width: 343, 
+    
+  },
+  title: {
+    fontSize: 24,
+    paddingTop: 16,
+    paddingBottom: 16,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
