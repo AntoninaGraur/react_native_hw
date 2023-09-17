9dc267f14beb742bc9e6525145c375e4af958079
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-09-17 16:07:15 +0200

    update App.js

--- App.js
+++ App.js
@@ -1,61 +1,64 @@
-
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import RegistrationScreen from "./Screens/RegistrationScreen";
+
 import "react-native-gesture-handler";
 
 import LoginScreen from "./Screens/LoginScreen";
-
 import Home from "./Screens/Home";
-
-
 import CommentsCreeen from "./Screens/CommentsScreen";
 import MapScreen from "./Screens/MapScreen";
-
+import RegistrationScreen from "./Screens/RegistrationScreen";
 
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { FIREBASE_AUTH } from "./FirebaseConfig";
+import { User, onAuthStateChanged } from "firebase/auth";
 import store from "./redux/store";
-
+import { useEffect, useState } from "react";
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(FIREBASE_AUTH, (user) => {
+      console.log("user:", user);
+      setUser(user);
+    });
+  }, []);
+
   return (
     <Provider store={store}>
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Registration">
-        <Stack.Screen
-          name="Registration"
-          component={RegistrationScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="LoginScreen"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
-       
-       
-       
-     
-        <Stack.Screen
-          name="CommentsScreen"
-          component={CommentsCreeen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="MapScreen"
-          component={MapScreen}
-          options={{ headerShown: false }}
-        />
-       
-      </Stack.Navigator>
-      </NavigationContainer>
-     </Provider>
+      <PersistGate
+        loading={<Text>Loading...</Text>}
+        persistor={store.persistor}
+      >
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Registration">
+            {user ? (
+              <Stack.Screen
+                name="Home"
+                component={Home}
+                options={{ headerShown: false }}
+              />
+            ) : (
+              <>
+                <Stack.Screen
+                  name="Registration"
+                  component={RegistrationScreen}
+                  options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                  name="LoginScreen"
+                  component={LoginScreen}
+                  options={{ headerShown: false }}
+                />
+              </>
+            )}
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 }
